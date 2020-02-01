f165a8dd9ca3b6606fa2de3191b5da3175f6fa1b
Marcial Soto <marcialsotosanchez@gmail.com>
2020-01-31 22:56:26 -0500

    ¨Prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC223UWmOZEOsijbsTjlZjHM-im78bZ6eo",
+    authDomain: "impulzor-iot.firebaseapp.com",
+    databaseURL: "https://impulzor-iot.firebaseio.com",
+    projectId: "impulzor-iot",
+    storageBucket: "impulzor-iot.appspot.com",
+    messagingSenderId: "818676739024",
+    appId: "1:818676739024:web:0b1875c35c1f733c3f27d5",
+    measurementId: "G-KTGE0X71NC"
+  }
 };
